7e244fba573e600301003312b1cb4dd597d73fcd
HugoDemont <hugodemont62@hotmail.fr>
2023-12-18 16:32:04 +0100

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,55 @@
-import React from 'react';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
+import {useLayoutEffect, useRef} from 'react';
+import './index.css';
 
 gsap.registerPlugin(ScrollTrigger);
 
-function App() {
-  // ---------- gsap context
-  React.useLayoutEffect(() => {
-    const ctx = gsap.context(() => {
-      // ---------- selecting all horizontal sections
-      const horizontalSections = gsap.utils.toArray('.horizontal-section');
+export default function App() {
+  const component = useRef();
+  const slider = useRef();
 
-      // ---------- applying horizontal scroll animation
-      gsap.to(horizontalSections, {
-        xPercent: -100 * (horizontalSections.length - 1),
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      let panels = gsap.utils.toArray('.panel');
+      gsap.to(panels, {
+        xPercent: -100 * (panels.length - 1),
         ease: 'none',
         scrollTrigger: {
-          trigger: '#container',
+          trigger: slider.current,
           pin: true,
           scrub: 1,
-          snap: 1 / (horizontalSections.length - 1),
-          end: () => '+=' + document.querySelector('#container').offsetWidth,
+          end: () => '+=' + slider.current.offsetWidth,
         },
       });
-    });
-
+    }, component);
     return () => ctx.revert();
-  }, []);
+  });
 
   return (
-    <main className="h-screen flex flex-nowrap" id="container">
-      {/* ---------- section 01 ---------- */}
-      <section className="horizonal-section bg-red-400 flex">
-        <h1 className="heading">Horizontal Scroll</h1>
-      </section>
-
-      {/* ---------- section 02 ---------- */}
-      <section className="horizonal-section bg-black flex">
-        <h1 className="heading">01</h1>
-      </section>
-
-      {/* ---------- section 02 ---------- */}
-      <section className="horizonal-section bg-amber-400 flex">
-        <h1 className="heading">02</h1>
-      </section>
-
-      {/* ---------- section 03 ---------- */}
-      <section className="horizonal-section bg-blue-400 flex">
-        <h1 className="heading">03</h1>
-      </section>
-    </main>
+    <div className="App" ref={component}>
+      <div ref={slider} className="container">
+        <div className="description panel blue">
+          <div>
+            <h1>
+              Yakuza Story
+            </h1>
+            <p>
+              L’origine des yakuzas remonte à l’ère Édo entre 1603 et 1868. En effet ils descendent de trois corporations différentes les bakuto ( joueurs professionnels qui travaillaient dans les villes les marchés et les foires dont la spécialité était les jeux de hasard)
+              Les tekiya ( les colporteurs qui voyagé sur les routes)
+              Et les machi-yokko ( groupes se considèrent comme protecteur du peuple lors de la démilitarisation du pays dans les années 1600 durant laquelle les attaques de ronins qui se multipliait)
+            </p>
+          </div>
+        </div>
+        <div className="panel ">ONE</div>
+        <div className="panel ">TWO</div>
+        <div className="panel ">THREE</div>
+        <div className="panel ">FOUR</div>
+        <div className="panel ">FIVE</div>
+        <div className="panel ">SIX</div>
+        <div className="panel ">SEVEN</div>
+        <div className="panel ">EIGHT</div>
+      </div>
+    </div>
   );
 }
-
-export default App;
